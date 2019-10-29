3ad09fbb0f8f45e7c6a48b7d738dff3e4f2e0cf4
Berkan Unal <Berkanunal@gmail.com>
2019-10-29 23:36:05 +0300

    Add getProductPrice function

--- src/get-data.ts
+++ src/get-data.ts
@@ -1,5 +1,32 @@
+import { convertPriceToNumber } from "./utils";
+
 export function getProductListElement() {
   return document.querySelector<HTMLUListElement>(
-    "#main-content > div.sidebar-plus-content > div > div > div:nth-child(6) > ul"
+    "main.segments > div.sidebar-plus-content > div.page-content > div.main > div:nth-child(6) > ul.products-listing"
   );
+}
+
+/**
+ * @param product: <li class="product-item">
+ */
+export function getProductPrice(product: HTMLLIElement) {
+  const prices = product.querySelector(
+    "article.hm-product-item > div.item-details > strong.item-price"
+  );
+
+  if (!prices) {
+    throw new Error(
+      `Prices for this product cant be found: ${product.innerHTML}`
+    );
+  }
+
+  const salePrice = (prices.querySelector(".sale") as HTMLSpanElement)
+    .innerText;
+  const regularPrice = (prices.querySelector(".regular") as HTMLSpanElement)
+    .innerText;
+
+  return {
+    salePrice: convertPriceToNumber(salePrice),
+    regularPrice: convertPriceToNumber(regularPrice)
+  };
 }
