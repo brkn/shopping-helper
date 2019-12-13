993cc8e19f7c5710a8c30c17b84ee38719bec384
Berkan Unal <Berkanunal@gmail.com>
2019-12-13 20:46:26 +0300

    store latestIndex

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import {
   getProductListElement,
-  getProductPrice
+  getProductPrice,
 } from "./data";
 import {getDiscount} from "./utils";
 
+let latestIndex = 0;
 const productListElement = getProductListElement();
-console.log(productListElement);
 
 if (productListElement) {
   const {children} = productListElement;
 
-  for (let i = 0; i < children.length; i += 1) {
+  for (let i = latestIndex; i < children.length; i += 1) {
     const child = children[i] as HTMLLIElement;
     const prices = getProductPrice(child);
 
@@ -18,11 +18,12 @@ if (productListElement) {
     // This should be the created element, could be useful at test:
     // <span class="price discount">50%</span>
     const discountElement = document.createElement("span");
-    const priceText = document.createTextNode(`%${getDiscount(prices.regularPrice, prices.salePrice)}`);
+    const priceText = document.createTextNode(
+      `%${getDiscount(prices.regularPrice, prices.salePrice)}`
+    );
     discountElement.setAttribute("class", "price discount");
     // TODO: instead of appendChild, use insertBefore
     discountElement.appendChild(priceText);
-
 
     const itemPriceElement = child.querySelector("strong.item-price");
 
@@ -30,4 +31,5 @@ if (productListElement) {
       itemPriceElement.appendChild(discountElement);
     }
   }
+  latestIndex = children.length - 1;
 }
